35e940993543441c07b843e9f9a4e01612dc7e39
ZhiHongZengTia <492298100@qq.com>
2024-08-27 04:10:22 -0700

    the first step

---
+++ README.txt
@@ -0,0 +1,1 @@
+this is git setup in Ubuntu 1804
---
+++ test1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+
+{
+	cout<<"the first program saved in Github./n";
+	return 0;
+}
+
+
